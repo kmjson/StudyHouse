27ae0b206115603f781256a3b82b769c31635a5c
Jarrett Fok <jarrettfok@gmail.com>
2023-03-16 21:32:02 -0400

    popup_tutorial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,14 @@
     import AuthStore from '../AuthStore';
     import Login from './Login.svelte';
     import Homepage from './Homepage.svelte';
+    import MyDialog from './myDialog.svelte';
 </script>
 
 <h1>StudyHouse</h1>
 
 {#if $AuthStore.isLoggedIn}
     <Homepage/>
+    <MyDialog/>
 {:else}
     <Login/>
 {/if}
