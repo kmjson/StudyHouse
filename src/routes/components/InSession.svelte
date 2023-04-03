dd465cfc05d1e1ae062cd831fab7b06e023bd589
Jarrett Fok <jarrettfok@gmail.com>
2023-04-02 21:08:55 -0400

    homepage-css-done

--- src/routes/components/InSession.svelte
+++ src/routes/components/InSession.svelte
@@ -59,13 +59,13 @@
     .button {
         display: block;
         margin: auto;
-        background-color: #F8DEC7; 
+        background-color: #DC83A4;
         border: none;
         font-family: "Porpora-Regular";
         font-size: 2rem;
-        padding: 0.5rem;
-        border-radius: 0.5rem;
-        color: #DC83A4;
+        padding: 1rem;
+        border-radius: 1.5rem;
+        color: #F8DEC7;
         cursor: pointer;
     }
 
