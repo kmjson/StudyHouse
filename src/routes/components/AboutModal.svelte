09f642c5ea4d0dc33fa0cbcb34cba7172d73380a
Jarrett Fok <jarrettfok@gmail.com>
2023-05-04 13:56:17 -0400

    about page done

--- src/routes/components/AboutModal.svelte
+++ src/routes/components/AboutModal.svelte
@@ -1,2 +1,75 @@
+<style>
+    .main {
+        overflow-y: scroll;
+        max-height: 70vh;
+    }
+
+    ::-webkit-scrollbar {
+        width: 0.4rem;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background: white;
+        border-radius: 0.2rem;
+    }
+    .big-image{
+        width: 25rem;
+    }
+    .small-image{
+        width: 15rem;
+    }
+    .super-small-image{
+        width: 9rem;
+    }
+    .super-super-small-image{
+        width: 5rem;
+    }
+</style>
+
 <h1>About StudyHouse!</h1>
-<h3>Coming Soon...</h3>
+
+<div class="main">
+    <h3>StudyHouse is a webapp and extension combo aimed to break the cycle of procrastination and build healthy work habits. Users who use StudyHouse earns coins each time in which they can use to purchase furnitures and decorations to customize their own house. StudyHouse offers this experience for all students who wish to stay on top of their work and never miss another deadline!
+    </h3>
+    
+    <h2 style="text-decoration: underline">How to use</h2>
+    <h3 style="text-decoration: underline">Download Extension</h3>
+    <h4>1. Navigate to <a href="https://github.com/kmjson/StudyHouse-Extension" target="_blank">https://github.com/kmjson/StudyHouse-Extension</a></h4>
+    <h4>2. Download the StudyHouse Extension by clicking the 'download ZIP' button</h4>
+    <img src="/about/asset_1.png" class="big-image">
+    <h4>3. Unzip the file</h4>
+    <h4>4. Navigate to Google Chrome extensions</h4>
+    <img src="/about/asset_2.png" class="small-image">
+    <h4>5. Click Unload 'Load unpacked'</h4>
+    <img src="/about/asset_3.png" class="big-image">
+    <h4>6. Select the folder 'StudyHouse-Extension-main'</h4>
+    <img src="/about/asset_4.png" class="big-image">
+    <h4>7. Enable the extension</h4>
+    <img src="/about/asset_5.png" class="small-image">
+    <h4>8. Pin the extension</h4>
+    <img src="/about/asset_6.png" class="small-image">
+    <br>
+    <h3 style="text-decoration: underline">Using the website</h3>
+    <h4>1. To begin the Study Session, click the 'Start Session' button</h4>
+    <img src="/about/asset-2_1.png" class="super-small-image">
+    <h4>2. Write the task you hope to accomplish. Click the submit button when ready</h4>
+    <img src="/about/asset-2_2.png" class="small-image">
+    <h4>3. Focus on your work and leave StudyHouse in the background</h4>
+    <h4>4. When you finish your Study Session, click the 'End Session' button</h4>
+    <img src="/about/asset-2_3.png" class="small-image">
+    <h4>5. Congratulations! You have just finished your first Study Session with StudyHouse!</h4>
+    <img src="/about/asset-2_4.png" class="small-image">
+    <br>
+    <h3 style="text-decoration: underline">Purchasing and placing furniture</h3>
+    <h4>1. To purchase furnitures, click the 'Store' button</h4>
+    <img src="/about/asset-3_1.png" class="super-super-small-image">
+    <h4>2. Click on the item you wish to purchas! In this case we'll buy Bed</h4>
+    <img src="/about/asset-3_2.png" class="small-image">
+    <h4>3. Now close the window and click on the 'Inventory' button </h4>
+    <img src="/about/asset-3_3.png" class="super-small-image">
+    <h4>4. Your recently purchased Bed is shown as an available option. Click 'Bed'. The furniture is successfully placed 
+        in the room if the Bed button is grayed out, displayed below </h4>
+    <img src="/about/asset-3_4.png" class="small-image">
+    <h4>5. Now close the window. Congratulations! you have successfully purchased and placed your first item </h4>
+    <img src="/about/asset-3_5.png" class="small-image">
+</div>
